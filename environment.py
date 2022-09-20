6c9cf93969528e7092b9c94bef086dfd3ef68015
Srinandini Marpaka <srinandinim@hotmail.com>
2022-09-19 22:51:11 -0400

    removed the case of too blocked

--- environment.py
+++ environment.py
@@ -8,7 +8,7 @@ class Environment:
 
     def __init__(self):
         self.generate_maze()
-        while not (self.validate_maze_blocking() and self.validate_maze_path()):
+        while not self.validate_maze():
             self.generate_maze()
 
     def generate_maze(self):
@@ -16,20 +16,7 @@ class Environment:
         self.maze[0][0].set_blocked(False)
         self.maze[Environment.SIZE - 1][Environment.SIZE - 1].set_blocked(False)
 
-    # based on the idea of 1.5 * .28 = .42
-    def validate_maze_blocking(self):
-        blocked_cells = 0
-        for j in range(Environment.SIZE):
-            for i in range(Environment.SIZE):
-                if (self.maze[i][j]).get_blocked() == True:
-                    blocked_cells+=1
-
-        if Environment.SIZE**2 * Environment.BLOCKING_PROBABILITY * 1.5 <= blocked_cells:
-            return False
-
-        return True
-
-    def validate_maze_path(self):
+    def validate_maze(self):
         directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
         visited = [[0 for x in range(Environment.SIZE)] for y in range(Environment.SIZE)] 
         queue = []
