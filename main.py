82b8165e13094faa200d20e8e4673a54352de1ab
tejpshah <55666467+tejpshah@users.noreply.github.com>
2022-09-24 17:21:35 -0400

    ghost class done

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
+from venv import create
 from environment import Environment
 from agent import Agent, Agent1 
+from ghost import Ghost 
 
 def create_game():
     env = Environment() 
     agent1 = Agent1() 
+    ghost = Ghost()
 
-if __name__ == '__main__':
-    env = Environment()
-    agent1 = Agent1()
-    path = (agent1.plan_path(env))
-    
+    path = agent1.plan_path(env)
 
     print(env)
     print(path)
 
+    for i in range(10):
+        print(ghost.get_location())
+        ghost.update_location(env)
+
     env.get_picture()
+
+if __name__ == '__main__':
+    create_game()
+
+
