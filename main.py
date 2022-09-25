5d570d1e785e64c9db0049537bc58272a4a660a0
tejpshah <55666467+tejpshah@users.noreply.github.com>
2022-09-25 16:32:34 -0400

    updates

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ if __name__ == '__main__':
     #run_agents.simulation_statistics_verbose_agent1(num_simulations=1, num_ghosts=5)
     #run_agents.simulation_statistics_verbose_agent2(num_simulations=1, num_ghosts=5)
 
-
     env = Environment(num_ghosts=1)
     agent3 = Agent3()
     agent3.run_agent3(env)
