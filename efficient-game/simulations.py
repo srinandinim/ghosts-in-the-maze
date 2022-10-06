db7b6d60360ed2a788d581714391f00e29fcc09d
tejpshah <55666467+tejpshah@users.noreply.github.com>
2022-10-06 18:02:32 -0400

    seperated out agents into seperate files

--- efficient-game/simulations.py
+++ efficient-game/simulations.py
@@ -1,6 +1,6 @@
 import random
 import constants 
-from agent import Agent1 
+from agent import Agent1, Agent2
 from environment import Environment 
 
 def simulation_statistics_agent1(num_simulations, num_ghosts):
@@ -33,6 +33,21 @@ def simulation_statistics_agent1_video(num_simulations, num_ghosts):
     print(f"Agent1: Wins: {wins}\tLosses: {losses}\tSurvival Rate: {round(survival*100,2)}%")  
     return round(survival*100,2)
 
+def simulation_statistics_agent2(num_simulations, num_ghosts):
+    """
+    run simulation n times and get statistics on survival, and more
+    """
+    rewards_agent1 = [] 
+    for _ in range(num_simulations):
+        env = Environment(num_ghosts=num_ghosts) 
+        agent2 = Agent2()
+        rewards_agent1.append(agent2.run_agent2(env))
+    wins = sum(rewards_agent1)
+    losses = len(rewards_agent1) - wins 
+    survival = wins / (wins + losses)
+    print(f"Agent2: Wins: {wins}\tLosses: {losses}\tSurvival Rate: {round(survival*100,2)}%")  
+    return round(survival*100,2)
+
 def lab_report_simulations(a1=False, a2=False, a3=False, a4=False, a5=False):
     """
     TODO: @Nandini, this needs to be updated to run on the same environment. 
@@ -48,9 +63,14 @@ def lab_report_simulations(a1=False, a2=False, a3=False, a4=False, a5=False):
 
     while last_survival_rate > 0 and num_ghosts < max_ghosts: 
         print(f"\nTHE NUMBER OF CURRENT GHOSTS ARE: {num_ghosts}")
+        
         if a1 == True: 
             a1_s = simulation_statistics_agent1(num_simulations=30, num_ghosts=num_ghosts)
             a1_stats[num_ghosts] = a1_s 
+    
+        if a2 == True:
+            a2_s = simulation_statistics_agent2(num_simulations=100, num_ghosts=num_ghosts)
+            a2_stats[num_ghosts] = a2_s
         
         last_survival_rate = min(last_survival_rate, max(a1_s, a2_s, a3_s, a4_s, a5_s))
         num_ghosts += 1
@@ -58,11 +78,14 @@ def lab_report_simulations(a1=False, a2=False, a3=False, a4=False, a5=False):
     return a1_stats, a2_stats, a3_stats, a4_stats, a5_stats
 
 if __name__ == "__main__":
-    #a1_stats, a2_stats, a3_stats, a4_stats, a5_stats = lab_report_simulations(a1=True)
-    #print(f"Agent 1 Stats: {a1_stats}")
+    """
+    a1_stats, a2_stats, a3_stats, a4_stats, a5_stats = lab_report_simulations(a1=True, a2=True)
+    print(f"Agent 1 Stats: {a1_stats}")
+    print(f"Agent 1 Stats: {a2_stats}")
+    """
 
-    """
-    
-    """
+    env = Environment(num_ghosts=5)
+    a2 = Agent2()
+    a2.run_agent2_debug(5)
 
 
