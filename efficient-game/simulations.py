0ad772c2bc57ce278b73b81f5c39e438e926a23e
Srinandini Marpaka <srinandinim@hotmail.com>
2022-10-08 19:22:33 -0400

    added runtime tracking

--- efficient-game/simulations.py
+++ efficient-game/simulations.py
@@ -1,3 +1,4 @@
+from cProfile import run
 import json
 import os
 import time
@@ -91,7 +92,9 @@ def simulation_statistics_agent3(num_simulations, num_ghosts, environments=[]):
     """
     rewards_agent3 = []
     timeouts = 0
+    runtimes = []
     for i in range(num_simulations):
+        start_time = time.time()
         env = deepcopy(environments[i]) if environments else Environment(
             num_ghosts=num_ghosts)
         agent3 = Agent3()
@@ -99,12 +102,13 @@ def simulation_statistics_agent3(num_simulations, num_ghosts, environments=[]):
         rewards_agent3.append(1 if result == 1 else 0)
         if result == -1:
             timeouts = timeouts + 1
+        runtimes.append(round(time.time() - start_time, 5))
     wins = sum(rewards_agent3)
     losses = len(rewards_agent3) - wins
     survival = wins / (wins + losses)
     print(
         f"Agent3: Wins: {wins}\tLosses: {losses}\tTimouts: {timeouts}\tSurvival Rate: {round(survival*100,2)}%")
-    return round(survival*100, 2), timeouts
+    return round(survival*100, 2), timeouts, runtimes
 
 
 def simulation_statistics_agent3_video(num_simulations, num_ghosts, environments=[]):
@@ -132,7 +136,9 @@ def simulation_statistics_agent4(num_simulations, num_ghosts, environments=[]):
     """
     rewards_agent4 = []
     timeouts = 0
+    runtimes = []
     for i in range(num_simulations):
+        start_time = time.time()
         env = deepcopy(environments[i]) if environments else Environment(
             num_ghosts=num_ghosts)
         agent4 = Agent4(env)
@@ -140,12 +146,13 @@ def simulation_statistics_agent4(num_simulations, num_ghosts, environments=[]):
         rewards_agent4.append(1 if result == 1 else 0)
         if result == -1:
             timeouts = timeouts + 1
+        runtimes.append(round(time.time() - start_time, 5))
     wins = sum(rewards_agent4)
     losses = len(rewards_agent4) - wins
     survival = wins / (wins + losses)
     print(
         f"Agent4: Wins: {wins}\tLosses: {losses}\tTimouts: {timeouts}\tSurvival Rate: {round(survival*100,2)}%")
-    return round(survival*100, 2), timeouts
+    return round(survival*100, 2), timeouts, runtimes
 
 
 def save_simulation_statistics(timestamp, a1_stats=None, a2_stats=None, a3_stats=None, a4_stats=None, a5_stats=None):
@@ -160,6 +167,18 @@ def save_simulation_statistics(timestamp, a1_stats=None, a2_stats=None, a3_stats
     with open(filename, 'w') as fp:
         json.dump(file_content, fp)
 
+def save_runtime_statistics(timestamp, a1_runtimes=None, a2_runtimes=None, a3_runtimes=None, a4_runtimes=None, a5_runtimes=None):
+    file_content = {'a1_runtimes': a1_runtimes, 'a2_runtimes': a2_runtimes,
+                    'a3_runtimes': a3_runtimes, 'a4_runtimes': a4_runtimes, 'a5_runtimes': a5_runtimes}
+
+    dirname = "experiments/"
+    if not os.path.exists(os.path.dirname(dirname)):
+        os.makedirs(os.path.dirname(dirname))
+
+    filename = "{}simulation_runtimes{}.json".format(dirname, timestamp)
+    with open(filename, 'w') as fp:
+        json.dump(file_content, fp)
+
 
 def visualize_simulation_statistics(timestamp):
     dirname = "experiments/"
@@ -171,9 +190,10 @@ def visualize_simulation_statistics(timestamp):
 
 def lab_report_simulations(a1=False, a2=False, a3=False, a4=False, a5=False):
     a1_stats, a2_stats, a3_stats, a4_stats, a5_stats = {}, {}, {}, {}, {}
+    a1_runtimes, a2_runtimes, a3_runtimes, a4_runtimes, a5_runtimes = {}, {}, {}, {}, {}
     last_survival_rate, num_ghosts, max_ghosts = 100, 1, constants.SIZE[0] * \
         constants.SIZE[1]
-    num_simulations = 30
+    num_simulations = 2
     a1_s = a2_s = a3_s = a4_s = a5_s = 0
 
     start_time = time.time()
@@ -195,17 +215,21 @@ def lab_report_simulations(a1=False, a2=False, a3=False, a4=False, a5=False):
             a2_stats[num_ghosts] = a2_s
 
         if a3 == True:
-            a3_s, timeouts = simulation_statistics_agent3(
+            a3_s, timeouts, runtimes = simulation_statistics_agent3(
                 num_simulations=num_simulations, num_ghosts=num_ghosts, environments=environments)
             a3_stats[num_ghosts] = [a3_s, timeouts]
+            a3_runtimes[num_ghosts] = runtimes
 
         if a4 == True:
-            a4_s, timeouts = simulation_statistics_agent4(
+            a4_s, timeouts, runtimes = simulation_statistics_agent4(
                 num_simulations=num_simulations, num_ghosts=num_ghosts, environments=environments)
             a4_stats[num_ghosts] = [a4_s, timeouts]
+            a4_runtimes[num_ghosts] = runtimes
 
         save_simulation_statistics(timestamp=start_time, a1_stats=a1_stats,
                                    a2_stats=a2_stats, a3_stats=a3_stats, a4_stats=a4_stats, a5_stats=a5_stats)
+        save_runtime_statistics(timestamp=start_time, a1_runtimes=a1_runtimes,
+                                a2_runtimes=a2_runtimes, a3_runtimes=a3_runtimes, a4_runtimes=a4_runtimes, a5_runtimes=a5_runtimes)
 
         last_survival_rate = min(
             last_survival_rate, max(a1_s, a2_s, a3_s, a4_s, a5_s))
@@ -219,7 +243,7 @@ def lab_report_simulations(a1=False, a2=False, a3=False, a4=False, a5=False):
 if __name__ == "__main__":
     # start = time.time()
     a1_stats, a2_stats, a3_stats, a4_stats, a5_stats = lab_report_simulations(
-        a1=False, a2=False, a3=True, a4=False)
+        a1=False, a2=False, a3=True, a4=True)
     # print(f"Agent 1 Stats: {a1_stats}")
     # print(f"Agent 2 Stats: {a2_stats}")
     # print(f"Agent 3 Stats: {a3_stats}")
